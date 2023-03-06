1090ffe8d803e6d73d295f6d9b6fb9f84f35760b
Ankita Kumari <114810670+ankitakri963@users.noreply.github.com>
2023-03-06 20:08:51 +0530

    Add files via upload

---
+++ Class3.php
@@ -0,0 +1,69 @@
+<?php
+
+// define('NAME','Anshu Kumar');
+// define('COLLEGE','BITM');
+// echo NAME;
+// echo '<br>';
+// echo COLLEGE;
+
+
+
+// $arr = array('NIT','MIT','BITM','SIT');
+// echo $arr[2];
+// echo "<br>";
+// for($i=0;$i<4;$i++){ 
+//     echo $arr[$i];
+//     echo "<br>";
+// }
+
+
+
+// $arr = array('name'=>'Anshu','college'=>'BITM','number'=>'9631289302','location'=>'Dubai');
+// foreach ($arr as $key=>$value){
+//     echo $key. "=>".$value;
+//     echo"<br>";
+// }
+
+
+
+// $arr = array(
+//     array('name'=>'Anshu','college'=>'BITM','number'=>'9631289302','location'=>'Dubai'),
+//     array('name'=>'Ankita','college'=>'BITM','number'=>'8102737541','location'=>'Kolkata'),
+// );
+// foreach ($arr as $a){
+//     foreach ($a as $key=>$value){
+//         echo $key. "=>".$value;
+//         echo"<br>";
+//     }
+//     echo "<br><br>";
+// }
+
+
+
+// class Vehicle{
+
+//     function getName(){
+//         return $this -> name;
+//     }
+//     function setName($name){
+//         $this -> name = $name;
+//     }
+//     function getColor(){
+//         return $this -> color;
+//     }
+//     function setColor($color){
+//         $this -> color = $color;
+//     }
+// }
+// $obj = new Vehicle();
+// $obj -> setName("Maruti");
+// $obj -> setColor("Pink");
+// echo $obj -> getName();
+// echo "<br>";
+// echo $obj -> getColor();
+// echo "<br>";
+
+
+
+?>
+
